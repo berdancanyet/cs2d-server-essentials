a3a26485110ea983668f51e5a00bd8a194f4c6eb
Berdan Can Yetkiner <13546853+berdancanyet@users.noreply.github.com>
2023-11-30 05:09:04 +0300

    Update main.lua

--- sys/lua/autorun/main.lua
+++ sys/lua/autorun/main.lua
@@ -1,11 +1,8 @@
 --Public Server Script and AWP & Shield Limiter by BcY. 04.2020.
 
-local tbought = false
-local ctbought = false
+local tbought, ctbought = false, false
 local shield = false
-local m = msg2
-local p = parse
-local t = timer
+local m, p, t = msg2, parse, timer
 local menuname = game("sv_name")
 
 addhook("serveraction","_svac")		
@@ -16,51 +13,43 @@ menu(id,""..tostring(menuname)..",Reset Score,,(Timeleft: "..game("mp_timelimit"
 end
 
 addhook("menu","men")
-function men(id,baslik,buton)
-	if baslik== tostring(menuname) then
-		if buton==1 then
-			if player(id,"score") > 0 or player(id,"deaths") > 0 then
-				p("setscore "..id.. " 0")
-				p("setdeaths "..id.. " 0")
-				msg2(id,"\169000255000You have successfully nullified your score!@C")
-			else
-				msg2(id,"Error: Your score is already nullified!@C")
-			end 
-		end 
-	end
+function men(id,title,button)
+    if title == menuname and button == 1 then
+        if player(id, "score") > 0 or player(id, "deaths") > 0 then
+            p("setscore " .. id .. " 0")
+            p("setassists " .. id .. " 0")
+            p("setdeaths " .. id .. " 0")
+            m(id, "\169000255000You have successfully nullified your score!@C")
+        else
+            m(id, "Error: Your score is already nullified!@C")
+        end
+    end
 end
 	
 addhook("startround","mneabled")
 function mneabled(id)
-	tbought = false
-	ctbought = false
-	shield = false
+	tbought, ctbought, shield = false, false, false
 end
 
 addhook("buy","_buy")
 function _buy(id,weapon)
-	if player(id,"team") == 1 and weapon == 35 and tbought == true then
-		p("sv_sound2 "..id.." env/click2.wav")
-		m(id,"Only one AWP per team allowed@C")
-		return 1 
-	elseif player(id,"team") == 1 and weapon == 35 and tbought == false then
-		tbought = true
-		return 0
-	end
-	if player(id,"team") == 2 and weapon == 35 and ctbought == true then
-		p("sv_sound2 "..id.." env/click2.wav")
-		m(id,"Only one AWP per team allowed@C")
-		return 1 
-	elseif player(id,"team") == 2 and weapon == 35 and ctbought == false then
-		ctbought = true
-		return 0
-	end
-	if player(id,"team") == 2 and weapon == 41 and shield == true then --make sure Tactical Shield is not in mp_unbuyable
-		p("sv_sound2 "..id.." env/click2.wav")
-		m(id,"Only one Tactical shield can be bought per round.@C")
-		return 1
-	elseif player(id,"team") == 2 and weapon == 41 and shield == false then
-		shield = true
-		return 0
-	end
+    local team = player(id, "team")
+    
+    if weapon == 35 and ((team == 1 and tbought) or (team == 2 and ctbought)) then
+        p("sv_sound2 " .. id .. " env/click2.wav")
+        m(id, "Only one AWP per team allowed@C")
+        return 1
+    elseif weapon == 35 and team == 1 then
+        tbought = true
+    elseif weapon == 35 and team == 2 then
+        ctbought = true
+    end
+    
+    if weapon == 41 and team == 2 and shield then
+        p("sv_sound2 " .. id .. " env/click2.wav")
+        m(id, "Only one Tactical shield can be bought per round.@C")
+        return 1
+    elseif weapon == 41 and team == 2 then
+        shield = true
+    end
 end
